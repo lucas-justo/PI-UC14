553a26c6de91a4440d3793ff7d04289f29e8d407
Jordano <jordanoffice97@gmail.com>
2019-04-12 11:37:52 -0300

    about aleterada

---
+++ footer.php
@@ -0,0 +1,13 @@
+<footer>
+<div class="container-fluid col-sm-8 contato"> 
+	 <div class="texto2 col-sm-4">
+		<a href="www.google.com"> <h1>FALE CONOSCO</h1></a>
+	 </div> 
+	 
+	 <div class="texto2 col-sm-4">
+		 <a href="www.google.com"><h1>TERMOS</h1></a>
+		 <a href="www.google.com"> <h1>E</h1></a>
+		  <a href="www.google.com"> <h1>CONDIÇÕES</h1></a>
+	 </div> 	 
+</div>
+</footer>
