6fd7f81963ea7602e66184ac4eec76fd8f91fbef
Jordano <jordanoffice97@gmail.com>
2019-04-15 11:23:50 -0300

    entrega e termos preenchidas

--- entregas.php
+++ entregas.php
@@ -7,7 +7,7 @@
 		<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Corben" />
 		<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
-<body class="teste">
+<body>
 
 	<?php
             require_once 'menu.php';
@@ -15,15 +15,29 @@
 		
 	<div class="container-fluid d-flex p-2 blog-container">
 	
-		<div class="blog">
-			<h1>Texto</h1>
-			<h4>Texto</h4>
-			<h4>Texto</h4>
-			<h4>Texto</h4>
-			<h4>Texto</h4>
-			<h4>Texto</h4>
-			<h4>Texto</h4>
+		<div class="blog"><br>
+			<h1>Entregas</h1>
+			<h4>Nosso sistema de entregas, é facil simples e descomplicado. Contamos com diversas formas de envio
+			e entrega, conforme demanda do cliente. Claro que tambem temos descontos especiais conforme o valor
+			da sua compra. Mas lembramos também, O cliente será responsável pelas perdas e danos causados 
+			pela recusa injustificada da entrega, assim considerada aquela não prevista na legislação aplicável.
+			O cliente poderá solicitar, no momento da formalização do pedido, o envio da mercadoria por meio 
+			dos Correios ou Transportadora, recordando que cada localidade detém um prazo específico para entrega.
+			 Algumas mercadorias, em razão da dimensão e do peso, estão sujeitas a alteração da forma de postagem 
+			 (por exemplo: TVs, Instrumentos Musicais, etc.).  Em caso de grandes eventos promocionais como 
+			 Black Friday e Cyber Monday, os prazos de entrega poderão sofrer alterações devido à grande demanda de postagens.</h4>
+			
 		</div>
 	</div>
+	
+	<div class="container-fluid correcao-promo promo"> 
+		<div class="texto">
+			<h1 class="foobar">SUPER PROMOÇÕES</h1>
+			<a href="www.google.com"> <h4 id="oferta" >Clique aqui e não perca! Só oferta boa Xuxuzinhos!</h4></a>
+		</div> 
+		<img class="sachikopromo" src="sachiko.png" /> 
+	</div>
+	
 </body>
+	<?php include("footer.php") ?>
 </html>
--- termos.php
+++ termos.php
@@ -7,30 +7,29 @@
 		<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Corben" />
 		<link rel="stylesheet" type="text/css" href="styles.css">
 </head>
-<body class="teste">
+<body>
 
 	<?php
             require_once 'menu.php';
         ?>
 		
 	<div class="container-fluid d-flex p-2 blog-container">
-		<div class="btn-group-vertical col-2">
-			<button class="btn btn-secondary btn-lg btn-blog"> <a href="info.php">    Informacoes  </a></button>
-			<button class="btn btn-secondary btn-lg btn-blog"> <a href="about.php">    Sobre Nos   </a></button>
-			<button class="btn btn-secondary btn-lg btn-blog"> <a href="entregas.php"> Entregas    </a></button>
-			<button class="btn btn-secondary btn-lg btn-blog"> <a href="priv.php">     Privacidade </a></button>
-			<button class="btn btn-secondary btn-lg btn-blog"> <a href="blog.php">     Blog        </a></button>
-		</div>	
 	
-		<div class="blog">
-			<h1>Texto</h1>
-			<h4>Texto</h4>
-			<h4>Texto</h4>
-			<h4>Texto</h4>
-			<h4>Texto</h4>
-			<h4>Texto</h4>
-			<h4>Texto</h4>
+		<div class="blog"><br>
+			<h1>Termos e Condições</h1><br>
+			<h4>As mesmas de qualquer outro e-commerce, só que com o dolphin na frente de
+			qualquer palavra! :D</h4>
 		</div>
 	</div>
+	
+	<div class="container-fluid correcao-promo promo"> 
+		<div class="texto">
+			<h1 class="foobar">SUPER PROMOÇÕES</h1>
+			<a href="www.google.com"> <h4 id="oferta" >Clique aqui e não perca! Só oferta boa Xuxuzinhos!</h4></a>
+		</div> 
+		<img class="sachikopromo" src="sachiko.png" /> 
+	</div>
+
 </body>
+	<?php include("footer.php") ?>
 </html>
