4ebe78c0c6906876ea3b75339bb20bb9f718ab35
Jordano Rodrigues Da Silva <181710122@senacinformatica.br>
2019-04-24 12:08:06 -0300

    correcoes

--- clientes.php
+++ clientes.php
@@ -7,9 +7,12 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="UTF-8">
-        <title>Market M171 - Clientes</title>
-    </head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Corben" />
+		<link rel="stylesheet" type="text/css" href="styles.css">
+	</head>
     <body>
         <?php
             require_once 'menu.php';
